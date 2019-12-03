362fb01498ec1ee5bc892c7adddf95a38f9f3d65
alsdede <andreluizsilveira.88@gmail.com>
2019-12-03 00:23:54 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { MdAddShoppingCart }from 'react-icons/md'
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+  <ProductList>
+      <li>
+          <img src ="https://static.netshoes.com.br/produtos/tenis-olympikus-fly-unissex/14/D22-2814-014/D22-2814-014_detalhe1.jpg?resize=280:280" alt ="Tênis"/>
+            <strong>Tênis muito legal</strong>
+            <span>R$129,90</span>
+            <button type="button">
+                <div>
+                    <MdAddShoppingCart size = {16} color="#FFF"/> 3
+                </div>
+                <span> ADICIONAR AO CARRINHO</span>
+            </button>
+      </li>
+      <li>
+          <img src ="https://static.netshoes.com.br/produtos/tenis-olympikus-fly-unissex/14/D22-2814-014/D22-2814-014_detalhe1.jpg?resize=280:280" alt ="Tênis"/>
+            <strong>Tênis muito legal</strong>
+            <span>R$129,90</span>
+            <button type="button">
+                <div>
+                    <MdAddShoppingCart size = {16} color="#FFF"/> 3
+                </div>
+                <span> ADICIONAR AO CARRINHO</span>
+            </button>
+      </li>
+      <li>
+          <img src ="https://static.netshoes.com.br/produtos/tenis-olympikus-fly-unissex/14/D22-2814-014/D22-2814-014_detalhe1.jpg?resize=280:280" alt ="Tênis"/>
+            <strong>Tênis muito legal</strong>
+            <span>R$129,90</span>
+            <button type="button">
+                <div>
+                    <MdAddShoppingCart size = {16} color="#FFF"/> 3
+                </div>
+                <span> ADICIONAR AO CARRINHO</span>
+            </button>
+      </li>
+      <li>
+          <img src ="https://static.netshoes.com.br/produtos/tenis-olympikus-fly-unissex/14/D22-2814-014/D22-2814-014_detalhe1.jpg?resize=280:280" alt ="Tênis"/>
+            <strong>Tênis muito legal</strong>
+            <span>R$129,90</span>
+            <button type="button">
+                <div>
+                    <MdAddShoppingCart size = {16} color="#FFF"/> 3
+                </div>
+                <span> ADICIONAR AO CARRINHO</span>
+            </button>
+      </li>
+      <li>
+          <img src ="https://static.netshoes.com.br/produtos/tenis-olympikus-fly-unissex/14/D22-2814-014/D22-2814-014_detalhe1.jpg?resize=280:280" alt ="Tênis"/>
+            <strong>Tênis muito legal</strong>
+            <span>R$129,90</span>
+            <button type="button">
+                <div>
+                    <MdAddShoppingCart size = {16} color="#FFF"/> 3
+                </div>
+                <span> ADICIONAR AO CARRINHO</span>
+            </button>
+      </li>
+      <li>
+          <img src ="https://static.netshoes.com.br/produtos/tenis-olympikus-fly-unissex/14/D22-2814-014/D22-2814-014_detalhe1.jpg?resize=280:280" alt ="Tênis"/>
+            <strong>Tênis muito legal</strong>
+            <span>R$129,90</span>
+            <button type="button">
+                <div>
+                    <MdAddShoppingCart size = {16} color="#FFF"/> 3
+                </div>
+                <span> ADICIONAR AO CARRINHO</span>
+            </button>
+      </li>
+  </ProductList>
+
+  );
+}
